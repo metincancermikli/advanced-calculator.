2c54ed2de54ee1746f9e6dfc573a1dbae19d054f
metincancermikli <156536275+metincancermikli@users.noreply.github.com>
2024-01-15 19:31:16 +0300

    Update advancedcalculator.py

--- advancedcalculator.py
+++ advancedcalculator.py
@@ -1,22 +1,49 @@
 #I will share better Python calculator code that I developed as a beginner.
 
-sayi_1 = float(input("Bir sayı giriniz: "))
+print("Hesap makinesine hoş geldiniz!")
+kullaniciadi = input("Lütfen bir kullanıcı adı belirleyiniz: ")
 
-ifade = input("yapılacak işlemi seçiniz: ")
+parola = input("Lütfen bir parola belirleyiniz: ")
 
-sayi_2 = float(input("İşlem yapılacak diğer sayıyı giriniz: "))
 
-if ifade == "+":
-    print("Seçtiğiniz işlem Toplama İşlemi(+) işleminizin sonucu:",sayi_1+sayi_2)
+if "abc" in parola:
+    print("Parolanız başarıyla belirlendi!")
+elif "abc" in kullaniciadi:
+    print("Kullanıcı adınız başarıyla belirlendi!")
+else:
+    print("Parola başarıyla belirlendi")
+    print("Giriş Ekranına yönlendiriliyorsunuz")
 
-elif ifade == "-":
-    print("Seçtiğiniz işlem Çıkarma İşlemi(-) işleminizin sonucu:",sayi_1-sayi_2)
+kadi = "metin123"
+sifre = "metin123"
 
-elif ifade == "*":
-    print("Seçtiğiniz işlem Çarpma İşlemi(*) işleminizin sonucu:",sayi_1*sayi_2)
+kullanici_adi = input("Lütfen Kullanıcı adınızı giriniz: ")
+sifre = input("Lütfen şifrenizi giriniz: ")
 
-elif ifade == "/":
-    print("Seçtiğiniz işlem Bölme İşlemi(/) işleminizin sonucu:",sayi_1/sayi_2)
+if kadi != kullanici_adi:
+    print("Lütfen kullanıcı adını doğru giriniz: ")
+    print("tekrar deneyiniz")
+elif sifre != sifre:
+    print("Şifre yanlış")
+    print("tekrar deneyiz")
 
-else :
-    print("Sadece +,-,*,/ ifadelerini seçmelisiniz!!! ") 
+else:
+    print("Giriş başarılı")
+    print("Hesap makinesine yönlendiriliyorsunuz")
+
+while True:
+    sayi_1 = float(input("Lütfen işlem yapılacak sayıyı giriniz: "))
+    ifade = input("İşlem yapılacak ifadeyi seçiniz ÖRN: +,-,*,/: ")
+    sayi_2 = float(input("İşlem yapılacak diğer sayıyı giriniz: "))
+
+    if ifade == "+":
+        print("Seçtiğiniz işlem toplama (+) İşlemi işleminizin sonucu",sayi_1+sayi_2)
+    elif ifade == "-":
+        print("Seçtiğiniz işlem Çıkartma (-) İşlemi işleminizin sonucu",sayi_1-sayi_2)
+    elif ifade == "*":
+        print("Seçtiğiniz işlem çarpma (*) İşlemi işleminizin sonucu",sayi_1*sayi_2)
+    elif ifade == "/":
+        print("Seçtiğiniz işlem bölme (/) İşlemi işleminizin sonucu",sayi_1/sayi_2)
+
+    else:
+        print("Sadece +,-,*,/ ifadelerini seçmelisiniz!!! ")
